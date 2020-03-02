4a5242178fb7052430ed7ad83bf3243d1bdd3a7a
Mellieejj <Melanieverheul1992@gmail.com>
2020-03-02 15:19:47 +0100

    encrypting, user model, router

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const databaseURL =
 
 const db = new Sequelize(databaseURL);
 
-db.sync()
+db.sync({ force: true })
   .then(() => console.log("database connected"))
   .catch(console.error);
 
--- user/router.js
+++ user/router.js
@@ -1,12 +1,19 @@
 const User = require("./model");
+const bcrypt = require("bcrypt");
 const { Router } = require("express");
 
 const router = Router();
 
 router.post("/user", async (request, response, next) => {
   try {
-    const user = await User.create(request.body);
-    response.send(user);
+    const encrypted = bcrypt.hashSync(request.body.password, 10);
+
+    const user = {
+      email: request.body.email,
+      password: encrypted
+    };
+    const person = await User.create(user);
+    response.send(person);
   } catch (error) {
     next(error);
   }
