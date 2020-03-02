e4cb4787b0da2b8cf69803e782fb175c81f1b355
Mellieejj <Melanieverheul1992@gmail.com>
2020-03-02 15:56:57 +0100

    login finalize

--- auth/router.js
+++ auth/router.js
@@ -1,5 +1,7 @@
 const { Router } = require("express");
 const { toJWT, toData } = require("./jwt");
+const bcrypt = require("bcrypt");
+const User = require("../user/model");
 
 const router = Router();
 
@@ -7,8 +9,32 @@ router.post("/login", (request, response, next) => {
   const { email, password } = request.body;
 
   if (email && password) {
-    const jwt = toJWT({ userId: 1 });
-    response.send({ jwt });
+    User.findOne({
+      where: {
+        email: email
+      }
+    })
+      .then(entity => {
+        if (!entity) {
+          response.status(400).send({
+            message: "User with that email does not exist"
+          });
+        } else if (bcrypt.compareSync(password, entity.password)) {
+          response.send({
+            jwt: toJWT({ userId: entity.id })
+          });
+        } else {
+          response.status(400).send({
+            message: "Password was incorrect"
+          });
+        }
+      })
+      .catch(error => {
+        console.error(error);
+        response.status(500).send({
+          message: "Something went wrong"
+        });
+      });
   } else {
     response.status(400).send({
       message: "Please fill in a valid email & password"
